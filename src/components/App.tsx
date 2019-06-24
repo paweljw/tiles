b11029547889c67df8282de4ad33834bf73cc242
Paweł J. Wal <p@steamshard.net>
2019-06-24 19:51:32 +0200

    Some types fun

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,14 +4,20 @@ import { inject } from 'mobx-react'
 import Loading from './Loading/Loading'
 
 interface IAppProps {
+  loading?: boolean
+}
+
+interface IAppFullProps {
   loading: boolean
 }
 
-const App = ({ loading }: IAppProps) => (
+const App = ({ loading }: IAppProps | IAppFullProps) => (
   <>
     {loading ? <Loading /> : null}
   </>
 )
+
+export { App as BaseApp }
 
 export default inject(({ gameStateStore }) => ({
   loading: gameStateStore

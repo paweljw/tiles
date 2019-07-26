2c71bc4ae4947b86d2d68fd84247404f239e35f9
Paweł J. Wal <p@steamshard.net>
2019-07-26 17:02:07 +0200

    Build 37

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -4,7 +4,6 @@ import { Simple as Cull } from './culling'
 
 import scaleToWindow from './helpers/scaleToWindow'
 import { CharacterContainer } from './containers/CharacterContainer'
-import Collider from './Collider'
 import stores from '../stores'
 import SkeletonContainer from './containers/SkeletonContainer'
 import LightProvider from './LightProvider'
